0d481805e076da851a29ef7740a8b6ec15bbc6fc
Ricardo Goitia <ricardogoitia108@gmail.com>
2025-10-14 10:33:02 -0400

    importante cambio de allowed host

--- SSAPI/settings.py
+++ SSAPI/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-di5^4t_*(zn&xd0i4i0ng!urql(_4e=jg830&3pe*n%zq(5730'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['https://activos.paldaca.com/', 'activos.paldaca.com']
-#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['activos.cpaldaca.com', 'www.activos.cpaldaca.com', 'localhost', '127.0.0.1']
+
 
 # Application definition
 
